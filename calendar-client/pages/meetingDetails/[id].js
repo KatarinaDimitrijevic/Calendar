92b0029c159213863683339249c772b495f12d3b
KatarinaDimitrijevic <katarina.dimitrijeviiic@gmail.com>
2022-04-18 17:06:03 +0200

    improved meeting details page

--- calendar-client/pages/meetingDetails/[id].js
+++ calendar-client/pages/meetingDetails/[id].js
@@ -7,9 +7,42 @@ const MeetingDetails = () => {
 
     console.log(id);
 
+    const [meeting, setMeeting] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:7000/api/meetings/" + id)
+        .then((res) => res.json())
+        .then((curr_meeting) => setMeeting(curr_meeting));
+    }, []);
+
+
+    const deleteMeeting = (meeting) => {
+        const options = {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+          query: JSON.stringify(id),
+        };
+    
+        fetch("http://localhost:7000/api/meetings/" + id, options)
+          .then(router.back());
+      };
+
+
+    const onClickDelete = () => {
+        deleteMeeting(meeting);
+    };
+
+
     return(
         <div>
-            Test
+            <h3>Meeting info:</h3>
+            <p>Title: {meeting.title}</p>
+            <p>Description: {meeting.title}</p>
+            <p>Time: {meeting.time}</p>
+            <div>
+                Participants: {meeting.participants}
+            </div>
+            <hr></hr>
+            <button onClick={() => onClickDelete()}>Delete</button>
         </div>
     )
 };
